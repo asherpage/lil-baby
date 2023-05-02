a9fc4f44a7301a9f1759adc3e33b861564843f43
asherpage <110576723+asherpage@users.noreply.github.com>
2023-05-02 16:13:28 -0700

    somewhat

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(window).on('load', function() {
+    console.log("hello")
 })
-$(function(){
- $('.box1').on('click', function(){
-  $(`.abt-start`).slideDown(1000);
+$(document).scroll(function() {
+           if($(window).scrollTop() > 950){
 
- })
-})
+            $("#abt-start").animate({"opacity": "1"},900).delay(2000);
+            $("#abt-start").animate({"transform": "perspective(70vw) rotateX(0) rotateY(30deg)"},900);
+            $("#abt-overlap").animate({"opacity": "1"},900);
+           }
+    });
