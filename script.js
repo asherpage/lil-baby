33a5d47795b01e7b78075ee216a443f643a54141
asherpage <ASHER.PAGE2121@GMAIL.COM>
2023-05-08 19:08:47 -0700

    l

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ $(window).on('load', function() {
     console.log("hello")
 })
 $(document).scroll(function() {
-           if($(window).scrollTop() > 910){
+           if($(window).scrollTop() > 900){
 
-            $("#abt-start").css({"opacity": "1"},1000);
-            $("#abt-overlap").css({"opacity": "1"},1000);
-            $("#abt-overlap2").css({"opacity": ".5"},1000);
+            $("#abt-start").animate({"opacity": "1"},2000);
+            // $("#abt-overlap").css({"opacity": "1"},1000);
+            // $("#abt-overlap2").css({"opacity": ".5"},1000);
            }
     });
+    $(document).scroll(function() {
+        if($(window).scrollTop() > 1400){
+
+         $("#abt-edu").animate({"opacity": "1"},2000);
+        }
+ });
